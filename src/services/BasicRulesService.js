c9fc530480d9d6f96da0ebbe08035ff431806dbb
IcyCaster <chester.booker.nz@gmail.com>
2018-04-25 18:39:16 +1200

    Added basic conways rules

--- src/services/BasicRulesService.js
+++ src/services/BasicRulesService.js
@@ -1,3 +1,13 @@
 const underPopulation = (neighbours) => {
-
+    return neighbours < 2;
 };
+
+const overPopulation = (neighbours) => {
+    return neighbours > 3;
+};
+
+const reproductive = (neighbours) => {
+    return neighbours === 3;
+};
+
+export default { underPopulation, overPopulation, reproductive }
--- src/services/BasicRulesService.test.js
+++ src/services/BasicRulesService.test.js
@@ -1,5 +1,52 @@
-describe('Addition', () => {
-    it('knows that 2 and 2 make 4', () => {
-        expect(2 + 2).toBe(4);
+import BasicRulesService from "./BasicRulesService";
+
+describe('BasicRulesService', () => {
+
+    describe('UnderPopulation', () => {
+        it('should return true if there are one of less neighbours', () => {
+            const neighbours = 1;
+            const isUnderPopulated = BasicRulesService.underPopulation(neighbours);
+            expect(isUnderPopulated).toBeTruthy();
+        });
+
+        it('should return false if there are two or more neighbours', () => {
+            const neighbours = 2;
+            const isUnderPopulated = BasicRulesService.underPopulation(neighbours);
+            expect(isUnderPopulated).toBeFalsy();
+        });
+    });
+
+    describe('OverPopulation', () => {
+        it('should return true if there are four or more neighbours', () => {
+            const neighbours = 4;
+            const isUnderPopulated = BasicRulesService.overPopulation(neighbours);
+            expect(isUnderPopulated).toBeTruthy();
+        });
+
+        it('should return false if there are three or less neighbours', () => {
+            const neighbours = 3;
+            const isUnderPopulated = BasicRulesService.overPopulation(neighbours);
+            expect(isUnderPopulated).toBeFalsy();
+        });
+    });
+
+    describe('Reproductive', () => {
+        it('should return true if there are exactly three neighbours', () => {
+            const neighbours = 3;
+            const isUnderPopulated = BasicRulesService.reproductive(neighbours);
+            expect(isUnderPopulated).toBeTruthy();
+        });
+
+        it('should return false if there are two or less neighbours', () => {
+            const neighbours = 2;
+            const isUnderPopulated = BasicRulesService.reproductive(neighbours);
+            expect(isUnderPopulated).toBeFalsy();
+        });
+
+        it('should return false if there are four or more neighbours', () => {
+            const neighbours = 4;
+            const isUnderPopulated = BasicRulesService.reproductive(neighbours);
+            expect(isUnderPopulated).toBeFalsy();
+        });
     });
 });
